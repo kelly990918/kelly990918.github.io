cd199bbc38adb56b1db1c23fc9965d2bc3719270
Kelly Leung <kelly.yyleung@gmail.com>
2024-12-10 14:41:44 -0500

    progress

--- college_travelers/script.js
+++ college_travelers/script.js
@@ -11,4 +11,10 @@ function hideModal(modalID) {
     document.getElementById('id01').style.display = "none";
 }
 
-// 
+function on() {
+  document.getElementById("id01").style.display = "block";
+}
+
+function off() {
+  document.getElementById("id01").style.display = "none";
+}
